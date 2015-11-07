aece08f64b80032ff04c4c487e91f547d23bf532
Jeremy Astolfi <jeremyjastolfi@gmail.com>
2015-11-07 18:30:41 -0500

    Added more work to Ractus, added needle controls

---
+++ Assets/Scripts/NeedleControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Vector3 collisionNormal = other.contacts[0].normal;
+            if (!m_player.dealDamage(m_damage, collisionNormal))
+            {
+                AddKnockback(collisionNormal * -1);
+            }
+        }
+    }
+}
