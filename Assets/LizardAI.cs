683f022c5542f868984332d60bdc5da10eba84b1
unknown <wsw3877@GDD-25.main.ad.rit.edu>
2015-11-06 21:33:22 -0500

    Added the 2d library into the scene. Created a player character that moves and shields. Created a wander volume that changes the direction of the enemy when they walk out of it. Created a lizard enemy that walks.

---
+++ Assets/LizardAI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class LizardAI : MonoBehaviour 
+{
+    [SerializeField] private float m_MaxSpeed = 10f; 
+
+    private Rigidbody2D m_Rigidbody2D;
+    private int direction = 1;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+
+    public void FixedUpdate()
+    {
+        move();
+    }
+
+    public void move()
+    {
+        //Debug.Log("speed" + m_MaxSpeed);
+        //Debug.Log("direction" + direction);
+        m_Rigidbody2D.velocity = new Vector2(m_MaxSpeed * direction, m_Rigidbody2D.velocity.y);
+    }
+
+    public void changeDirections()
+    {
+        direction *= -1;
+    }
+}
---
+++ Assets/WanderZoneTriggerCode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderZoneTriggerCode : MonoBehaviour 
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<LizardAI>().changeDirections();
+        }
+    }
+}
