246436a3a2432680f3dab724d925d3a3a2957cd2
unknown <wsw3877@GDD-25.main.ad.rit.edu>
2015-11-07 16:01:09 -0500

    Moved more movement things up to the base class. Added knockback to the lizard for when the shield is up.

--- Assets/Scripts/LizardAI.cs
+++ Assets/Scripts/LizardAI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 //using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets._2D;
@@ -6,17 +7,18 @@ using UnityStandardAssets._2D;
 public class LizardAI : MovingObject 
 {
     [SerializeField] private float m_ChargeSpeed = 20f; 
+    [SerializeField] private float m_MaxChargeSpeed = 20f;
     [SerializeField] private int m_sightRadius = 10;
     [SerializeField] private float m_chargeCooldown = .5f;
 
     private PlatformerCharacter2D m_player;
     private Rigidbody2D m_playerRigidBody2D;
     private Transform sightline;
-    private Vector2 direction;
     private bool m_charging = false;
     private bool m_canCharge = true;
     private bool m_queuedDirectionChange = false;
     private int m_damage = 1;
+    private bool m_stunned = false;
     
 
 	// Use this for initialization
@@ -25,9 +27,7 @@ public class LizardAI : MovingObject
         base.Awake();
         m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerCharacter2D>();
         m_playerRigidBody2D = m_player.GetComponent<Rigidbody2D>();
-        direction = m_Rigidbody2D.transform.right;
         sightline = m_Rigidbody2D.transform.FindChild("Sightline").transform;
-        m_health = 1;
 	}
 	
 	// Update is called once per frame
@@ -38,7 +38,7 @@ public class LizardAI : MovingObject
 
     public void FixedUpdate()
     {
-        move();
+        Move(direction.x, false);
         if (m_queuedDirectionChange)
         {
             m_queuedDirectionChange = false;
@@ -46,26 +46,36 @@ public class LizardAI : MovingObject
         }
     }
 
-    public void move()
+    public override void Move(float move, bool jump)
     {
-        if (m_charging)
+        if (!m_stunned)
         {
-            m_Rigidbody2D.velocity = new Vector2(m_ChargeSpeed * direction.x, m_Rigidbody2D.velocity.y);
-        }
-        else
-        {
-            m_Rigidbody2D.velocity = new Vector2(m_MaxSpeed * direction.x, m_Rigidbody2D.velocity.y);
+            if (m_charging)
+            {
+                m_Rigidbody2D.AddForce(m_Rigidbody2D.velocity * friction * -1);
+
+                if (move * m_Rigidbody2D.velocity.x < m_MaxSpeed)
+                {
+                    m_Rigidbody2D.AddForce(Vector2.right * move * m_ChargeSpeed);
+                }
+
+                if (Math.Abs(m_Rigidbody2D.velocity.x) > m_MaxSpeed)
+                {
+                    m_Rigidbody2D.velocity = new Vector2(Mathf.Sign(m_Rigidbody2D.velocity.x) * m_MaxChargeSpeed, m_Rigidbody2D.velocity.y);
+                }
+            }
+            else
+            {
+                base.Move(move, jump);
+            }
         }
     }
 
-    public void changeDirections()
+    protected override void Flip()
     {
         if (!m_charging)
         {
-            direction *= -1;
-            Vector3 position = sightline.localPosition;
-            position.x *= -1;
-            sightline.localPosition = position;
+            base.Flip();
         }
         else
         {
@@ -73,12 +83,21 @@ public class LizardAI : MovingObject
         }
     }
 
+    public void changeDirections()
+    {
+        Flip();
+    }
+
     void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             Vector3 collisionNormal = other.contacts[0].normal;
-            m_player.dealDamage(m_damage, collisionNormal);
+            if (!m_player.dealDamage(m_damage, collisionNormal))
+            {
+                AddKnockback(collisionNormal*-1);
+                m_stunned = true;
+            }
             ChangeChargeState(false);
         }
     }
@@ -104,9 +123,10 @@ public class LizardAI : MovingObject
         m_canCharge = true;
     }
 
-    public override void dealDamage(int dmg, Vector3 collisionNormal)
+    public override bool dealDamage(int dmg, Vector3 collisionNormal)
     {
         m_health -= dmg;
         CheckIfDead();
+        return true;
     }
 }
--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -1,23 +1,61 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using UnityStandardAssets._2D;
 
 public abstract class MovingObject : MonoBehaviour 
 {
     [SerializeField] protected int m_health;
     [SerializeField] protected Vector2 m_knockbackForce = new Vector2(500, 500);
-    [SerializeField] protected float m_MaxSpeed;              // The fastest the player can travel in the x axis.
+    [SerializeField] protected float m_MaxSpeed = 10;              // The fastest the player can travel in the x axis.
     [SerializeField] protected float m_knockbackTime = 0.1f;
+    [SerializeField] protected float friction = 2f;
+    [SerializeField] protected float speed = 400f;
+    [SerializeField] protected bool m_AirControl = true;                 // Whether or not a player can steer while jumping;
 
     protected Rigidbody2D m_Rigidbody2D;
     protected bool m_FacingRight = true;  // For determining which way the player is currently facing.
     protected bool m_isKnockedback = false;
+    protected Vector2 direction;
+    protected bool m_Grounded = true;            // Whether or not the player is grounded.
 
     protected virtual void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        direction = m_Rigidbody2D.transform.right;
     }
 
-    protected void Flip()
+    public virtual void Move(float move, bool jump)
+    {
+        if (!m_isKnockedback && (m_Grounded || m_AirControl))
+        {
+            m_Rigidbody2D.AddForce(m_Rigidbody2D.velocity * friction * -1);
+
+            if (move * m_Rigidbody2D.velocity.x < m_MaxSpeed)
+            {
+                m_Rigidbody2D.AddForce(Vector2.right * move * speed);
+            }
+
+            if (Math.Abs(m_Rigidbody2D.velocity.x) > m_MaxSpeed)
+            {
+                m_Rigidbody2D.velocity = new Vector2(Mathf.Sign(m_Rigidbody2D.velocity.x) * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+            }
+
+            if (move > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (move < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+    }
+
+    protected virtual void Flip()
     {
         // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
@@ -26,9 +64,11 @@ public abstract class MovingObject : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+        direction *= -1;
+        Debug.Log("flip direction " + direction);
     }
 
-    public abstract void dealDamage(int dmg, Vector3 collisionNormal);
+    public abstract bool dealDamage(int dmg, Vector3 collisionNormal);
 
     protected virtual void CheckIfDead()
     {
